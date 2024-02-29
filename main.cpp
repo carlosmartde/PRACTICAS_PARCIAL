9cd90207578c57310a691bb0cf8ab8c0c9346ab9
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-29 10:21:03 -0600

    DESCUENTOS

--- main.cpp
+++ main.cpp
@@ -3,23 +3,20 @@ using namespace std;
 
 
 int main() {
-        int calificacion;
-        cout<<"INGRESE LA CALIFICACION NUMERICA DEL ESTUDIANTE ENTRE 0-100:  "<<endl;
-        cin>>calificacion;
+      int precio;
+      int porcentaje;
+      int descuento;
+      int precio2;
+      cout<<"INGRESE EL PRECIO DEL PRODUCTO: "<<endl;
+      cin>>precio;
+      cout<<"INGRESE EL PORCENTAJE DE DESCUENTO: "<<endl;
+      cin>>porcentaje;
 
-        if (calificacion>=90 && calificacion<=100){
-            cout<<"CALIFICACION 'A'"<<endl;
-        }else if(calificacion>=80 && calificacion<=89){
-            cout<<"CALIFICACION 'B'"<<endl;
-        }else if(calificacion>=70 && calificacion<=79) {
-            cout << "CALIFICACION 'C'" << endl;
-        }else if(calificacion>=60 && calificacion<=69) {
-            cout << "CALIFICACION 'D'" << endl;
-        }else if(calificacion>=00 && calificacion<=59) {
-            cout << "CALIFICACION 'F'" << endl;
-        }else{
-            cout<<"LA NOTA NUMERICA INGRESADA NO ES VALIDA"<<endl;
-        }
+      descuento=precio*porcentaje/100;
+      precio2=precio-descuento;
+
+      cout<<"EL PRECIO DESPUES DEL DESCUENTO ES: "<<precio2<<" USTED TUVO UN AHORRO DE: "<<descuento<<endl;
+
 
     return 0;
 }
