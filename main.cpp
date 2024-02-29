8a450a469c112715a8cfb4dadec85e65d21bf7ea
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-28 21:35:15 -0600

    NUMERO MAYOR ENTRE 3 NUMEROS

--- main.cpp
+++ main.cpp
@@ -3,15 +3,22 @@ using namespace std;
 
 
 int main() {
-    int numero;
-    cout<<"ingrese el numero:"<<endl;
-    cin>>numero;
+   double numero1, numero2, numero3;
 
-    if (numero%2 ==0){
-        cout<<"el numero es par"<<endl;
-    }else {
-    cout<<"el numero es impar"<<endl;
-    }
+
+   cout<<"INGRESE EL PRIMER NUMERO: "<<endl;
+   cin>>numero1;
+   cout<<"INGRESE EL SEGUNDO NUMERO"<<endl;
+   cin>>numero2;
+   cout<<"INGRESE EL TERCER NUMERO: "<<endl;
+   cin>>numero3;
+
+   if(numero1>=numero2 && numero1>=numero3) {
+       cout << numero1 << " ES EL NUMERO MAYOR" << endl;
+   }else if(numero2>=numero1 && numero2>=numero3){
+       cout<<numero2<<" ES EL NUMERO MAYOR"<<endl;
+   }else
+       cout<<numero3<<" ES EL NUMERO MAYOR"<<endl;
 
     return 0;
 }
