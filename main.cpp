e9363fa086352ba14de74d506e3a2c798f6f88e4
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-29 10:39:12 -0600

    SUMA DE NUMEROS POSITIVOS

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main() {
 
     int numero;
-    int i=1;
-
-    cout<<"INGRESA UN NUMERO: "<<endl;
+    int suma=0;
+    cout<<"INGRESE UN NUMERO POSITIVO PARA SUMARLO O UNO NEGATIVO SI DESEA DETENER LA SUMA: "<<endl;
     cin>>numero;
-    while(i<=numero){
-        cout<<i<<endl;
-        i++;
+
+    while(numero>=0){
+        suma +=numero;
+        cout<<"SUMA PARCIAL: "<<suma<<endl;
+        cout<<"INGRESA OTRO NUMERO POSITIVO PARA AGREGARLO A LA SUMA O UNO NEGATIVO PARA DETENER LA SUMA: "<<endl;
+        cin>>numero;
     }
+    cout<<"LA SUMA DE LOS NUMEROS POSITIVOS ES :"<<suma<<endl;
 
 
     return 0;
