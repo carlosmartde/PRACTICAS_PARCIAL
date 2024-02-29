ad6c5485853917cfa0f4a91a035876c5e1ec119d
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-28 22:16:38 -0600

    CALIFICACIONES

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 
 int main() {
-        int numero;
-        cout<<"INGRESE EL NUMERO: "<<endl;
-        cin>>numero;
+        int calificacion;
+        cout<<"INGRESE LA CALIFICACION NUMERICA DEL ESTUDIANTE ENTRE 0-100:  "<<endl;
+        cin>>calificacion;
 
-        if (numero%5 ==0 && numero%7 !=0){
-            cout<<numero<<" ES UN MULTIPLO DE 5"<<endl;
-        }else if (numero%7 ==0 && numero%5 !=0){
-            cout<<numero<<" ES UN MULTIPLO DE 7"<<endl;
-        }else if (numero%5==0 && numero%7==0){
-            cout<<numero<<" ES UN MULTIPLO DE 5 Y DE 7"<<endl;
-        }else {
-            cout<<numero<<" NO ES MULTIPLO DE 5 NI DE 7"<<endl;
+        if (calificacion>=90 && calificacion<=100){
+            cout<<"CALIFICACION 'A'"<<endl;
+        }else if(calificacion>=80 && calificacion<=89){
+            cout<<"CALIFICACION 'B'"<<endl;
+        }else if(calificacion>=70 && calificacion<=79) {
+            cout << "CALIFICACION 'C'" << endl;
+        }else if(calificacion>=60 && calificacion<=69) {
+            cout << "CALIFICACION 'D'" << endl;
+        }else if(calificacion>=00 && calificacion<=59) {
+            cout << "CALIFICACION 'F'" << endl;
+        }else{
+            cout<<"LA NOTA NUMERICA INGRESADA NO ES VALIDA"<<endl;
         }
+
     return 0;
 }
