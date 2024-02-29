741cc0b3979f0fd370f739a91d09481018ef08b5
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-29 10:30:38 -0600

    numeros ascendentes con while

--- main.cpp
+++ main.cpp
@@ -3,20 +3,14 @@ using namespace std;
 
 
 int main() {
-      int precio;
-      int porcentaje;
-      int descuento;
-      int precio2;
-      cout<<"INGRESE EL PRECIO DEL PRODUCTO: "<<endl;
-      cin>>precio;
-      cout<<"INGRESE EL PORCENTAJE DE DESCUENTO: "<<endl;
-      cin>>porcentaje;
 
-      descuento=precio*porcentaje/100;
-      precio2=precio-descuento;
+      int i=1;
 
-      cout<<"EL PRECIO DESPUES DEL DESCUENTO ES: "<<precio2<<" USTED TUVO UN AHORRO DE: "<<descuento<<endl;
-
+     while (i<=20)
+     {
+         cout<<i<<endl;
+            i++;
+     }
 
     return 0;
 }
