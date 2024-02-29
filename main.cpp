fa228c240a98c395754ce6d44be147bfecbd32e9
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-28 21:43:30 -0600

    AÑOS BISIESTOS

--- main.cpp
+++ main.cpp
@@ -3,22 +3,13 @@ using namespace std;
 
 
 int main() {
-   double numero1, numero2, numero3;
-
-
-   cout<<"INGRESE EL PRIMER NUMERO: "<<endl;
-   cin>>numero1;
-   cout<<"INGRESE EL SEGUNDO NUMERO"<<endl;
-   cin>>numero2;
-   cout<<"INGRESE EL TERCER NUMERO: "<<endl;
-   cin>>numero3;
-
-   if(numero1>=numero2 && numero1>=numero3) {
-       cout << numero1 << " ES EL NUMERO MAYOR" << endl;
-   }else if(numero2>=numero1 && numero2>=numero3){
-       cout<<numero2<<" ES EL NUMERO MAYOR"<<endl;
-   }else
-       cout<<numero3<<" ES EL NUMERO MAYOR"<<endl;
-
+   int año;
+   cout<<"INGRESE EL ANIO: "<<endl;
+   cin>>año;
+   if ((año%4 ==0 && año%100 !=0)|| (año%400 ==0)){
+       cout<<año<<" SI ES UN ANIO BISIESTO"<<endl;
+   }else{
+       cout<<año<<" NO ES UN ANIO BISIESTO"<<endl;
+   }
     return 0;
 }
