8eba45832c0164638c8a62676fa2cecdc9ea8c15
carlos humberto martinez de leòn <carlosmartde@gmail.com>
2024-02-29 18:38:28 -0600

    PROMEDIO DE NUMEROS

--- main.cpp
+++ main.cpp
@@ -4,18 +4,26 @@ using namespace std;
 
 int main() {
 
-    int numero;
-    int i=2;
-    int suma;
+    int cantdenumeros;
+    double suma=0.0;
+    double numero;
+    int i=1;
 
-    cout<<"INGRESE UN NUMERO: "<<endl;
-    cin>>numero;
+    cout<<"INGRESE LA CANTIDAD DE NUMEROS QUE DESEA PROMEDEAR: "<<endl;
+    cin>>cantdenumeros;
 
-    while(i<=numero){
-        suma=i+i;
-        cout<<i<<endl;
-        i+=2;
+    if (cantdenumeros<=0){
+        cout<<"LA CANTIDAD DE NUMEROS DEBE DE SER AL MENOS 1: "<<endl;
+        return 1;
+            }
+    while(i<=cantdenumeros){
+        cout<<"INGRESE EL NUMERO "<<i<<" :"<<endl;
+        cin>>numero;
+        suma +=numero;
+        i++;
     }
+    double media= suma / cantdenumeros;
+    cout<<"EL PROMEDIO DE LOS NUMEROS ES: "<<media;
 
     return 0;
 }
